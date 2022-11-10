bf119d7f0f72b798a1d0fd19088d353a054da22c
idriss.naji <idriss.naji@virtual-identity.com>
2022-11-10 15:35:47 +0100

    fix:VIC-1891 Fix Swagger UI after CVEs fixes

---
+++ src/main/java/de/caritas/cob/videoservice/api/controller/CustomSwaggerApiResourceController.java
@@ -0,0 +1,20 @@
+package de.caritas.cob.videoservice.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
+import springfox.documentation.swagger.web.ApiResourceController;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+@Controller
+@ApiIgnore
+@RequestMapping(value = "${springfox.docuPath}" + "/swagger-resources")
+public class CustomSwaggerApiResourceController extends ApiResourceController {
+
+  public static final String SWAGGER_UI_BASE_URL = "/videocalls/docs";
+
+  public CustomSwaggerApiResourceController(SwaggerResourcesProvider swaggerResources) {
+    super(swaggerResources, SWAGGER_UI_BASE_URL);
+  }
+
+}
