3cca7120da7348b9a9aba9386cab9659390ccd91
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-05-10 15:40:44 +0200

    fix: init tables script

--- src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
+++ src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
@@ -7,7 +7,7 @@ CREATE TABLE videoservice.`videoroom` (
   `jitsi_room_id` bigint(21) NOT NULL,
   `video_link` varchar(1024) COLLATE utf8_unicode_ci NOT NULL,
   `create_date` datetime NOT NULL DEFAULT (UTC_TIMESTAMP),
-  `close_date` datetime NULL
+  `close_date` datetime NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
