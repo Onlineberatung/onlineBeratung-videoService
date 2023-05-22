a6624bd43b1f3a19d0e2cb9945096a75e4356d86
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-05-22 16:06:06 +0200

    fix: separate endpoint for stop video call event handling

--- src/main/java/de/caritas/cob/videoservice/config/SpringFoxConfig.java
+++ src/main/java/de/caritas/cob/videoservice/config/SpringFoxConfig.java
@@ -55,7 +55,7 @@ public class SpringFoxConfig {
       new String[] {
         "/videocalls/docs",
         "/videocalls/docs/**",
-        "/videocalls/event/stop/**",
+        "/videocalls/event/stop",
         "/v2/api-docs",
         "/configuration/ui",
         "/swagger-resources/**",
