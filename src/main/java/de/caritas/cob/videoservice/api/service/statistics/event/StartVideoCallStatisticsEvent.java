a9f39839a30322c6757919ece5b029bb79d9ba4f
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-09-14 16:47:22 +0200

    fix: pass tenantId for startVideoCall event

--- src/main/java/de/caritas/cob/videoservice/api/service/statistics/event/StartVideoCallStatisticsEvent.java
+++ src/main/java/de/caritas/cob/videoservice/api/service/statistics/event/StartVideoCallStatisticsEvent.java
@@ -7,10 +7,12 @@ import de.caritas.cob.videoservice.statisticsservice.generated.web.model.EventTy
 import de.caritas.cob.videoservice.statisticsservice.generated.web.model.StartVideoCallStatisticsEventMessage;
 import de.caritas.cob.videoservice.statisticsservice.generated.web.model.UserRole;
 import java.util.Optional;
+import lombok.AllArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class StartVideoCallStatisticsEvent implements StatisticsEvent {
 
   private static final EventType EVENT_TYPE = EventType.START_VIDEO_CALL;
@@ -20,6 +22,8 @@ public class StartVideoCallStatisticsEvent implements StatisticsEvent {
   private @NonNull Long sessionId;
   private @NonNull String videoCallUuid;
   private @NonNull String adviceSeekerId;
+
+  private Long tenantId;
 
   /** {@inheritDoc} */
   @Override
